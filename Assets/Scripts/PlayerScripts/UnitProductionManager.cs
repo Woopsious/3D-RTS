60346a68ce3ce7c91927c74403d3191ef645bcc9
Woopsious <reecehand@hotmail.co.uk>
2023-08-12 19:48:04 +0100

    issues from yesterday seems to be fixed and working fine after changing one line of code from "   obj.GetComponent<NetworkObject>().SpawnWithOwnerShip(clientId);" to "obj.GetComponent<NetworkObject>().Spawn(true);" my best guess is because unit has a component ClientTranform on it when the ServerRPC tries to move the obj it cant as its owned by the client, and ClientTranform blocks it??

--- Assets/Scripts/PlayerScripts/UnitProductionManager.cs
+++ Assets/Scripts/PlayerScripts/UnitProductionManager.cs
@@ -346,31 +346,23 @@ public class UnitProductionManager : NetworkBehaviour
 
 		VehProdSpawnLocation spawnLocationScript = NetworkManager.Singleton.SpawnManager.SpawnedObjects[buildingNetworkObjId].
 			GetComponent<VehProdSpawnLocation>();
-
-		///client 2 units spawn at the center of the map (Example on 50x50 map)
-		///Unit Spawn Coords = (45, 5, 45) | Unit Coords = (45, 5, 45), unit coords will be correct for 100 logs
-		///Unit Spawn Coords = (45, 5, 45) | Unit Coords = (25, 5, 25), suddenly its sent to the center of the map for some reason
 		Vector3 vehSpawnPos = spawnLocationScript.vehProdSpawnPoint.transform.position;
 
 		if (clientId == 0)
 		{
 			StartCoroutine(OpenCloseDoors(spawnLocationScript));
 			GameObject obj = Instantiate(GameManager.Instance.PlayerOneUnitsList[buildOrderIndex], vehSpawnPos, Quaternion.identity);
-			obj.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
+			obj.GetComponent<NetworkObject>().Spawn(true);
 			playerController.gameUIManager.techTreeManager.ApplyTechUpgradesToNewUnits(obj);
 			StartCoroutine(ChangeBuiltUnitState(obj.GetComponent<NetworkObject>().NetworkObjectId, destination));
-
-			//Debug.LogWarning("Unit Spawn Coords" + vehSpawnPos);
 		}
 		else if (clientId == 1)
 		{
 			StartCoroutine(OpenCloseDoors(spawnLocationScript));
 			GameObject obj = Instantiate(GameManager.Instance.PlayerTwoUnitsList[buildOrderIndex], vehSpawnPos, Quaternion.identity);
-			obj.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
+			obj.GetComponent<NetworkObject>().Spawn(true);
 			playerController.gameUIManager.techTreeManager.ApplyTechUpgradesToNewUnits(obj);
 			StartCoroutine(ChangeBuiltUnitState(obj.GetComponent<NetworkObject>().NetworkObjectId, destination));
-
-			//Debug.LogWarning("Unit Spawn Coords" + vehSpawnPos);
 		}
 	}
 }
